dec3d76bb2da07e9e3b4400748d89d37b5f0e37f
serge1997 <serge.gogolo12@gmail.com>
2024-10-07 22:57:23 -0300

    applicant form request

--- app/Http/Requests/ApplicantCreateRequest.php
+++ app/Http/Requests/ApplicantCreateRequest.php
@@ -11,7 +11,7 @@ class ApplicantCreateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,16 @@ class ApplicantCreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'fullname'           => ['required','string'],
+            'email'              => ['nullable'],
+            'username'           => ['required'],
+            'password'           => ['nullable'],
+            'cni_number'         => ['nullable'],
+            'attestation_number' => ['nullable'],
+            'phone'              => ['required'],
+            'address_id'         => ['nullable'],
+            'address_number'     => ['nullable'],
+            'address_complement' => ['nullable'],
         ];
     }
 }
